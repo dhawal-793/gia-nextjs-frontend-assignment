3d2bf1e3e8849f11e5df08162fd8cf572a21511e
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-01 16:08:05 +0530

    Created Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image"
+import Link from "next/link"
+import SearchBox from "./SearchBox"
+import Account from "./Account"
+
+const Navbar = () => {
+    return (
+        <div className="flex items-center w-full h-[52px] px-2 sm:px-3 z-50 bg-gray-primary ">
+            <div className="grid px-2 pr-3 place-items-center sm:pr-7 w-fit">
+                <Link href="/" className="inline-block relative w-[50px] h-[34px]">
+                    <Image src="/logowithname.png" fill alt="Go India Advisiors Logo" />
+                </Link>
+            </div>
+            <div className="flex flex-1">
+                <SearchBox />
+                <div className="flex items-center w-fit md:pr-2 md:gap-[0.8rem] lg:gap-5 sm:w-[38%] md:w-[42%] justify-end">
+                    <Link href="/contact" className="hidden md:block">Contact Us</Link>
+                    <Account />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
