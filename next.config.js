df5fabd0ec2f801550ef3e4bcf8d08e107ee67d8
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-01 15:45:48 +0530

    Added image domains in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['s3.ap-south-1.amazonaws.com']
+    }
+}
 
 module.exports = nextConfig
