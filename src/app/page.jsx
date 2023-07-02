1d3e56aa3b6af1c1f82121fbd94a096869789d41
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-02 08:48:53 +0530

    Added page.jsx

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+
+  return (
+    <main className="flex w-full overflow-x-hidden overflow-y-auto page-min-h sm:page-min-h-sm ">
+      Home page
+    </main>
+  )
+}
