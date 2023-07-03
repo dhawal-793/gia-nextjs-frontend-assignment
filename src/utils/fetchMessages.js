ea32335add8cb74bbf246aa45e894b03078b1dcd
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-03 09:15:09 +0530

    Created methods to fetch stories & duscussion data

---
+++ src/utils/fetchMessages.js
@@ -0,0 +1,23 @@
+import messages from '@/data/discussion.json';
+
+export const fetchMessages =  () => {
+    try {
+        //     const response = await fetch('https://raw.githubusercontent.com/dhawal-793/gia-assignment-dummy-data/main/discussion.json');
+        //     if (!response.ok) {
+        //         console.error(response);
+        //         throw new Error('Failed to fetch dummy data');
+        //     }
+        //     console.log(response);
+        //     const data = await response.json();
+        //     console.log(data);
+        //     return data;
+
+
+        // await new Promise(resolve => setTimeout(resolve, 1000));
+        return messages
+    } catch (error) {
+        console.error(error);
+        // Handle the error appropriately in your code
+    }
+}
+
---
+++ src/utils/fetchStories.js
@@ -0,0 +1,23 @@
+import stories from '@/data/marketStories.json';
+
+export const fetchStories =  () => {
+    try {
+        //     const response = await fetch('https://raw.githubusercontent.com/dhawal-793/gia-assignment-dummy-data/main/discussion.json');
+        //     if (!response.ok) {
+        //         console.error(response);
+        //         throw new Error('Failed to fetch dummy data');
+        //     }
+        //     console.log(response);
+        //     const data = await response.json();
+        //     console.log(data);
+        //     return data;
+
+
+        // await new Promise(resolve => setTimeout(resolve, 1000));
+        return stories
+    } catch (error) {
+        console.error(error);
+        // Handle the error appropriately in your code
+    }
+}
+
