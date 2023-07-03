934b7dda7bcfbb55d59b1d97e99c96b5936a0b0e
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-07-03 09:26:52 +0530

    Added meta tags, and dummy div after children

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,12 +3,26 @@ import './globals.css'
 import BottomMenu from '@/components/BottomMenu'
 import Header from '@/components/Header'
 
+export const metadata = {
+  title: 'GIA Coding Assignment',
+  description: 'Go india Advisors Coding Assignment for Next Js Fronend Developer Role',
+  authors: [
+    {
+      name: "Dhawal Vijayvargiya",
+      url: "https://dev-folio-793.vercel.app"
+    }
+  ]
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className='relative w-screen bg-gray-primary'>
         <Header />
-        {children}
+        <>
+          {children}
+          <div className='h-[42px] xxs:h-[60px] xs:h-0'/>
+        </>
         <BottomMenu />
         <Add />
       </body>
